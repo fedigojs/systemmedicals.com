7be8124764b222f658f4f7fd9a28e55b21eb21bf
Igor Fedianin <fedigo.sj@gmail.com>
2025-07-15 08:11:56 +0300

    fix: fix main page

--- src/components/main-page-card.jsx
+++ src/components/main-page-card.jsx
@@ -5,11 +5,11 @@ export default function MainPageCard() {
 
     return (
         <section className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-16">
-            <Link href="/category/stomatology">
+            <Link href="/category/dental-equipment">
                 <div
                     className="rounded-xl p-6 flex items-center justify-between bg-cyan-600 text-white hover:shadow-lg transition-shadow cursor-pointer">
                     <div className="max-w-[60%]">
-                        <h3 className="text-2xl font-bold mb-2">Stomatology</h3>
+                        <h3 className="text-2xl font-bold mb-2">Dental equipment</h3>
                             <p className="text-sm mb-4">Catalog of medical equipment at great prices</p>
                             <Button variant="outline" className="bg-white text-cyan-700 hover:bg-gray-100">
                                 View
@@ -19,11 +19,11 @@ export default function MainPageCard() {
                 </div>
             </Link>
 
-            <Link href="/category/general-medicine">
+            <Link href="/category/medical-equipment">
                 <div
                     className="rounded-xl p-6 flex items-center justify-between bg-yellow-300 text-slate-900 hover:shadow-lg transition-shadow cursor-pointer">
                     <div className="max-w-[60%]">
-                        <h3 className="text-2xl font-bold mb-2">General Medicine</h3>
+                        <h3 className="text-2xl font-bold mb-2">Medical equipment</h3>
                         <p className="text-sm mb-4">Catalog of medical equipment at great prices</p>
                         <Button variant="outline" className="bg-white text-yellow-800 hover:bg-yellow-100">
                             View
