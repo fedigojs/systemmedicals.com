0425c309b19ed6334b7c3b039e2140e620cf8d6c
Igor Fedianin <fedigo.sj@gmail.com>
2025-08-31 18:52:27 +0300

    fix: change a om link

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,10 +1,8 @@
 import {SidebarInset, SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar";
 import {AppSidebar} from "@/components/app-sidebar";
 import {Separator} from "@/components/ui/separator";
-import {
-    Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator
-} from "@/components/ui/breadcrumb";
 import AdminAuthGuard from "@/components/admin-auth-guard";
+import Link from "next/link";
 
 export default function AdminLayout({children}) {
 
@@ -16,9 +14,13 @@ export default function AdminLayout({children}) {
                     <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
                         <SidebarTrigger className="-ml-1"/>
                         <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4"/>
-                        <a href="/" className="ml-auto underline">
+                        <Link
+                            href="/"
+                            className="ml-auto underline"
+                            title="Click to go to the homepage"
+                        >
                              return to the homepage Systemmedicals.com
-                        </a>
+                        </Link>
                     </header>
                     {children}
                 </SidebarInset>
